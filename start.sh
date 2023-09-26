94c9657fca38e24954a2723748f44b8751cad26e
Leonardo Marcelino <leonardo.marcelino@goodlpay.com>
2023-09-26 13:31:05 +0100

    Initial commit

---
+++ start.sh
@@ -0,0 +1,19 @@
+# building the projects
+projects=("go-url-shortener" "go-albums") 
+
+for project in ${projects[@]}; do
+  echo "Building project: $project image..."
+  docker build -t "leonardom/$project-app" "https://github.com/leonardom/$project.git"
+  minikube image load "leonardom/$project-app"
+done
+
+echo "Deploying $project in minikube..."
+kubectl apply -f ./k8s --recursive
+
+for project in ${projects[@]}; do
+  echo "Exposing service $project..."
+  minikube service "$project-app-service" --url &
+done
+#kubectl port-forward "deployment/$project-app" 8000:8000 
+#minikube service go-albums-service --url
+
